6f054729efb9bf7b1845b787a85329afef959332
Million D <milliondejene@protonmail.com>
2024-08-03 01:08:50 +0300

    mimnor changes

--- routes/auth.js
+++ routes/auth.js
@@ -26,9 +26,8 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
   console.log('User logged in:', req.user);
   res.redirect(`https://main--glowing-sherbet-2fba6c.netlify.app?user=${encodeURIComponent(JSON.stringify(req.user))}`);
 });
-
-// Check authentication status
 router.get('/check-auth', (req, res) => {
+  console.log('User isAuthenticated:', req.isAuthenticated());
   if (req.isAuthenticated()) {
     res.status(200).json({ isAuthenticated: true, user: req.user });
   } else {
@@ -46,7 +45,6 @@ router.get('/logout', (req, res) => {
 router.options('*', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.sendStatus(204);
 });
